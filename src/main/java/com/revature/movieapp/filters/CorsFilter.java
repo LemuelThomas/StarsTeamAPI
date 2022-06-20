374f0351ee1df312ebf8464088bdb0f2cf0309a9
Gerard E. Paul <gerardpaul@outlook.com>
2022-06-20 12:14:40 -0500

    Basic project structure

---
+++ src/main/java/com/revature/movieapp/filters/CorsFilter.java
@@ -0,0 +1,25 @@
+package com.revature.movieapp.filters;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter
+@Component
+public class CorsFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        chain.doFilter(request, response); // if you don't put this, your controller/servlet never gets the request
+    }
+}
---
+++ src/main/java/com/revature/movieapp/models/Taskexample.java
@@ -0,0 +1,8 @@
+package com.revature.movieapp.models;
+
+public class Taskexample
+{ private String id;
+    private String faq_question;
+    private String faq_answer;
+
+}
