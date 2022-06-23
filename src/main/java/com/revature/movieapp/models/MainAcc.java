753b0fdc8b46b79376a15349cd02cba06e73c706
LemuelThomas <lemuel.thomas3@gmail.com>
2022-06-23 17:14:03 -0400

    Created a models and connections to the db

---
+++ src/main/java/com/revature/movieapp/models/MainAcc.java
@@ -0,0 +1,143 @@
+package com.revature.movieapp.models;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+@Entity
+@Table(name = "stars_account")
+public class MainAcc  {
+    @Column(name = "subscription_id", nullable = false)
+    private int sub_id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = true)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private int age;
+    @Id
+    @Column(name = "account_id",nullable = false)
+    private int id;
+
+    @OneToMany
+    private List<Users> accUsers;
+
+    public MainAcc() {
+    }
+
+    public MainAcc(int sub_id, String firstName, String lastName, String email, String username, String password, int age, int id) {
+        this.sub_id = sub_id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.id = id;
+    }
+
+    public int getSub_id() {
+        return sub_id;
+    }
+
+    public void setSub_id(int sub_id) {
+        this.sub_id = sub_id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Users> getAccUsers() {
+        return accUsers;
+    }
+
+    public void setAccUsers(List<Users> accUsers) {
+        this.accUsers = accUsers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MainAcc mainAcc = (MainAcc) o;
+        return sub_id == mainAcc.sub_id && age == mainAcc.age && id == mainAcc.id && Objects.equals(firstName, mainAcc.firstName) && Objects.equals(lastName, mainAcc.lastName) && Objects.equals(email, mainAcc.email) && Objects.equals(username, mainAcc.username) && Objects.equals(password, mainAcc.password) && Objects.equals(accUsers, mainAcc.accUsers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sub_id, firstName, lastName, email, username, password, age, id, accUsers);
+    }
+
+    @Override
+    public String toString() {
+        return "MainAcc{" +
+                "sub_id=" + sub_id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", id=" + id +
+                ", accUsers=" + accUsers +
+                '}';
+    }
+}
---
+++ src/main/java/com/revature/movieapp/models/Users.java
@@ -0,0 +1,67 @@
+package com.revature.movieapp.models;
+
+import javax.persistence.*;
+import java.util.Objects;
+@Entity
+@Table(name = "account_user")
+public class Users {
+    @Column(nullable = false)
+    private String username;
+
+    @Id
+    private int user_id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id",nullable = false)
+    private MainAcc userAcc;
+
+    public Users() {
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public MainAcc getUserAcc() {
+        return userAcc;
+    }
+
+    public void setUserAcc(MainAcc userAcc) {
+        this.userAcc = userAcc;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "username='" + username + '\'' +
+                ", user_id=" + user_id +
+                ", userAcc=" + userAcc +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return user_id == users.user_id && Objects.equals(username, users.username) && Objects.equals(userAcc, users.userAcc);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, user_id, userAcc);
+    }
+}
